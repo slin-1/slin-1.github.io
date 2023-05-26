cd83e91ead3a1d1cc1b9ca4c9a040c2cf6f4929b
Steven <105820635+flarobin@users.noreply.github.com>
2023-05-25 21:49:37 -0400

    Add sticky header color change

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,11 @@
 /* 
   Steven Lin's Portfolio
 */
+
+
+// Enables header to change background colors after scrolling
+window.onscroll = () => {
+  let header = document.querySelector('header');
+
+  header.classList.toggle('sticky', window.scrollY > 250);
+}
