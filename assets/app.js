158ce370b2c4091786d2dfa1ba07f04be5ac9478
Steven <105820635+flarobin@users.noreply.github.com>
2023-05-27 18:10:48 -0400

    Allow header nav-bar to detect 'active-tab'

--- assets/app.js
+++ assets/app.js
@@ -2,10 +2,29 @@
   Steven Lin's Portfolio
 */
 
+// Header navigation
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-// Enables header to change background colors after scrolling
 window.onscroll = () => {
-  let header = document.querySelector('header');
+  // Updates the header navigation's 'active-tab'
+  for (const section of sections) {
+    let id = section.getAttribute('id');
+    let top = window.scrollY;
+    let offset = section.offsetTop - 120;
+    let height = section.offsetHeight;
 
+    if ((top >= offset) && (top < (offset + height))) {
+      for (const link of navLinks) {
+        link.classList.remove('active-tab');
+        document.querySelector('header nav a[href*=' + id + ']')
+          .classList.add('active-tab');
+      }
+    }
+
+  }
+
+  // Enables header background color change after scrolling
+  let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 250);
 }
